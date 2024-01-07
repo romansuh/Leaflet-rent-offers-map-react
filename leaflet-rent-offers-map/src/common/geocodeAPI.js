149b52f9a8c43e720dfdcedf02fd00ce7acd853c
romansuh <72558747+romansuh@users.noreply.github.com>
2024-01-07 18:24:07 +0200

    getting address by coordinates function created added function to get addres by passing coordinates for rent offer cards

---
+++ leaflet-rent-offers-map/src/common/geocodeAPI.js
@@ -0,0 +1,3 @@
+export const API_KEY = "ge-fceb28f57dfc85c1";
+
+export const API_BASE_URL = "https://api.geocode.earth/v1/"
--- leaflet-rent-offers-map/src/components/RentOffersCards/RentOffersCards.js
+++ leaflet-rent-offers-map/src/components/RentOffersCards/RentOffersCards.js
@@ -1,4 +1,42 @@
 import {useSelector} from "react-redux";
+import "./RentOffersCards.css";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {API_BASE_URL, API_KEY} from "../../common/geocodeAPI";
+
+const fetchAddress = async (coords) => {
+    const url = API_BASE_URL + `reverse?point.lat=${coords[0]}&point.lon=${coords[1]}&api_key=${API_KEY}`;
+    const response = await axios.get(url);
+    const data = response.data;
+    const address = data.features[0].properties.label;
+    return address;
+};
+
+const RentOfferCard = ({ offerInfo }) => {
+    const [address, setAddress] = useState('');
+
+    useEffect(() => {
+        const getAddress = async () => {
+            try {
+                const result = await fetchAddress(offerInfo.geocode);
+                setAddress(result);
+            } catch (error) {
+                console.error('Error fetching address:', error);
+            }
+        };
+
+        getAddress();
+    }, [offerInfo.geocode]);
+
+    return (
+        <li key={offerInfo.id}>
+            <div  key={offerInfo.id} className="info_card_container">
+                <span  key={offerInfo.id}>{address}</span>
+            </div>
+        </li>
+    );
+};
+
 
 const RentOffersCards = () => {
     const rentOffers = useSelector(state => state.offers.offers);
@@ -7,7 +45,7 @@ const RentOffersCards = () => {
         <div className="info_cards_container">
             <ul className="info_cards_list">
                 {rentOffers.map(rentOffer => {
-                    return rentOffer.selected ? <li key={rentOffer.id}>{rentOffer.id}</li> : ''
+                    return rentOffer.selected ? <RentOfferCard offerInfo={rentOffer}/> : ''
                 })}
             </ul>
         </div>
